4e4d145d8207d79aaf67612ae21e92018b706390
BrightLight <ml@silverseed.de>
2018-08-18 11:41:23 +0200

    Added demo project to live-test and debug

---
+++ demo/SpellcheckerDemo/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpellcheckerDemo
+{
+    public class Class1
+    {
+        [DisplayName("Name1")]
+        [Display(Name ="Infrmation 1")]
+        public string Namez1 { get; }
+
+        [DisplayName("Name1")]
+        public string Name2 { get; }
+
+        [DisplayName("Informtion 1")]
+        public string Info1 { get; }
+
+        [DisplayName("Name1")]
+        public string Info2 { get; set; }
+    }
+}
