b2f5eaccaacf510a9f1e89e24bbec60050aa65a6
BrightLight <ml@silverseed.de>
2024-04-10 17:15:23 +0200

    Improve and extend tests for ClassNameSpellcheckAnalyzer
    Mainly as an example how to write these tests. Should also modernize all code inside the "Verifiers" folder

--- source/YouShouldSpellcheck.Analyzer.Test/ClassNameSpellcheckAnalyzerTests.cs
+++ source/YouShouldSpellcheck.Analyzer.Test/ClassNameSpellcheckAnalyzerTests.cs
@@ -1,46 +1,128 @@
 ﻿namespace YouShouldSpellcheck.Analyzer.Test
 {
+  using System.Diagnostics.CodeAnalysis;
+  using System.Threading;
+  using System.Threading.Tasks;
   using AnalyzerFromTemplate2019.Test;
   using Microsoft.CodeAnalysis;
-  using Microsoft.CodeAnalysis.Diagnostics;
   using Microsoft.CodeAnalysis.Testing;
   using NUnit.Framework;
-  using System.Threading.Tasks;
 
   /// <summary>
   /// Test class for the <see cref="ClassNameSpellcheckAnalyzer"/>.
   /// </summary>
+  /// <remarks>
+  /// The tested source code is defined as a class field instead of within the individual test methods
+  /// because <see cref="StringSyntaxAttribute"/> only works on fields and properties. 
+  /// </remarks>
   [TestFixture]
   public class ClassNameSpellcheckAnalyzerTests
   {
-    // Diagnostic triggered and checked for
-    [Test]
-    public async Task TestMethod2()
-    {
-      var test = @"
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Diagnostics;
-
-    namespace ConsoleApplication1
-    {
-           // 'TypeName' spelled wrong
+    /// <summary>
+    /// The source code with a spelling mistake in the class name.
+    /// This is used by <see cref="ReportsSpellingMistakesInClassNameWithExpectedMessage"/>.
+    /// </summary>
+    [StringSyntax("C#")]
+    private const string TypoInClassNames = """
+      using System;
+      using System.Collections.Generic;
+      using System.Linq;
+      using System.Text;
+      using System.Threading.Tasks;
+      using System.Diagnostics;
+                                                                                                                          
+      namespace ConsoleApplication1
+      {
+        // 'TypeName' spelled wrong
         class TypName
-        {   
-           public void PrintNow()
-           {
-           }
+        {
+          public void PrintNow()
+          {
+          }
+
+          // ClassNameSpellcheckAnalyzer should report on nested classes
+          private class Persn { };
         }
-    }";
+      }
+      """;
 
-      var expected = new DiagnosticResult("YS103", DiagnosticSeverity.Warning)
+    [StringSyntax("C#")]
+    private const string TypoInClassName = """
+      using System;
+      using System.Collections.Generic;
+      using System.Linq;
+      using System.Text;
+      using System.Threading.Tasks;
+      using System.Diagnostics;
+                                                                                                                          
+      namespace ConsoleApplication1
+      {
+        // 'TypeName' spelled wrong
+        class {|YS103:Typ|}Name
+        {
+          // ClassNameSpellcheckAnalyzer does not report on method names
+          public void PrntNow()
+          {
+          }
+        }
+      }
+      """;
+
+    [StringSyntax("C#")]
+    private const string TypoInNestedClassName = """
+      using System;
+      using System.Collections.Generic;
+      using System.Linq;
+      using System.Text;
+      using System.Threading.Tasks;
+      using System.Diagnostics;
+                                                                                                                          
+      namespace ConsoleApplication1
+      {
+        class TypeName
+        {
+          // ClassNameSpellcheckAnalyzer does not report on method names
+          public void PrntNow()
+          {
+          }
+         
+          // ClassNameSpellcheckAnalyzer should report on nested classes
+          private class {|YS103:Persn|} { };
+        }
+      }
+      """;
+
+    /// <summary>
+    /// Test to ensure that the analyzer detects a spelling mistake in the class name.
+    /// </summary>
+    [TestCase(TypoInClassName)]
+    [TestCase(TypoInNestedClassName)]
+    public async Task DetectsSpellingMistake(string sourcecode)
+    {
+      await CSharpAnalyzerVerifier<ClassNameSpellcheckAnalyzer>.VerifyAnalyzerAsync(sourcecode);
+    }
+
+    /// <summary>
+    /// Test to ensure that the analyzer reports a spelling mistake in a class name with the expected message.
+    /// </summary>
+    [Test]
+    public async Task ReportsSpellingMistakesInClassNameWithExpectedMessage()
+    {
+      var expected1 = new DiagnosticResult("YS103", DiagnosticSeverity.Warning)
         .WithMessage("Possible spelling mistake: Typ")
-        .WithLocation("/0/Test0.cs", 12, 15);
+        .WithLocation("/0/Test0.cs", 11, 9);
 
-      await CSharpAnalyzerVerifier<ClassNameSpellcheckAnalyzer>.VerifyAnalyzerAsync(test, expected);
+      var expected2 = new DiagnosticResult("YS103", DiagnosticSeverity.Warning)
+        .WithMessage("Possible spelling mistake: Persn")
+        .WithLocation("/0/Test0.cs", 18, 19);
+
+      var test = new CSharpAnalyzerVerifier<ClassNameSpellcheckAnalyzer>.Test
+      {
+        TestCode = TypoInClassNames,
+        ExpectedDiagnostics = { expected1, expected2 },
+      };
+
+      await test.RunAsync(CancellationToken.None);
     }
   }
 }
