8ea29fad7dd3a5fb4ec26764d5a47265bc10cf5b
BrightLight <ml@silverseed.de>
2024-05-08 16:53:18 +0200

    Move content of UnitTests.cs into ClassNameSpellcheckAnalyzerTests

--- source/YouShouldSpellcheck.Analyzer.Test/ClassNameSpellcheckAnalyzerTests.cs
+++ source/YouShouldSpellcheck.Analyzer.Test/ClassNameSpellcheckAnalyzerTests.cs
@@ -7,6 +7,7 @@
   using Microsoft.CodeAnalysis;
   using Microsoft.CodeAnalysis.Testing;
   using NUnit.Framework;
+  using YouShouldSpellcheck.Analyzer.CodeFixes;
 
   /// <summary>
   /// Test class for the <see cref="ClassNameSpellcheckAnalyzer"/>.
@@ -123,6 +124,73 @@
       };
 
       await test.RunAsync(CancellationToken.None);
+    }
+
+    /// <summary>
+    /// No diagnostics expected for empty string.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+    [Test]
+    public async Task NoDiagnosticsForEmptyString()
+    {
+      var test = @"";
+
+      await CSharpAnalyzerVerifier<ClassNameSpellcheckAnalyzer>.VerifyAnalyzerAsync(test);
+    }
+
+    //Diagnostic and CodeFix both triggered and checked for
+    [Test]
+    public async Task TestMethod2()
+    {
+
+      var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        // 'TypeName' spelled wrong
+        class TypName
+        {   
+        }
+    }";
+      var expected = new DiagnosticResult("YS103", DiagnosticSeverity.Warning)
+        .WithMessage("Possible spelling mistake: Typ")
+        .WithSpan("/0/Test0.cs", 12, 15, 12, 18);
+      ////.WithLocation("Test0.cs", 12, 15);
+
+      await CSharpAnalyzerVerifier<ClassNameSpellcheckAnalyzer>.VerifyAnalyzerAsync(test, expected);
+
+      var fixtest = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        // 'TypeName' spelled wrong
+        class TypeName
+        {   
+        }
+    }";
+
+      var testIt = new CSharpCodeFixVerifier<ClassNameSpellcheckAnalyzer, ClassNameCodeFixProvider>.Test
+      {
+        TestCode = test,
+        FixedCode = fixtest,
+        CodeActionIndex = 6,
+      };
+
+      testIt.ExpectedDiagnostics.Add(expected);
+      await testIt.RunAsync(CancellationToken.None);
+
     }
   }
 }
