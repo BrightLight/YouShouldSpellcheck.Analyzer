c0de34b0c68e526284f8c93e69e5d8100103c7a8
BrightLight <ml@silverseed.de>
2024-05-08 16:10:19 +0200

    Add documentation

--- source/YouShouldSpellcheck.Analyzer/LanguageTool/LanguageToolClient.cs
+++ source/YouShouldSpellcheck.Analyzer/LanguageTool/LanguageToolClient.cs
@@ -4,8 +4,24 @@
   using System.Threading.Tasks;
   using RestSharp;
 
+  /// <summary>
+  /// Client for the LanguageTool API.
+  /// </summary>
+  /// <remarks>
+  /// The LanguageTool API is a public API that allows you to check text for spelling and grammar mistakes.
+  /// This is only used for string literals (and maybe XML comments in the future), because these represent
+  /// whole sentences and are more likely to contain grammar mistakes. Obviously, grammar is not an issue
+  /// for identifiers, so we don't check those.
+  /// </remarks>
   public class LanguageToolClient
   {
+    /// <summary>
+    /// Checks the specified <paramref name="text"/> for spelling and grammar mistakes.
+    /// </summary>
+    /// <param name="localLanguageToolUri">The URI of the local LanguageTool server. </param>
+    /// <param name="text">The text to check. </param>
+    /// <param name="language">The language of the text.</param>
+    /// <returns>The response from the LanguageTool server.</returns>
     public static async Task<LanguageToolResponse> Check(Uri localLanguageToolUri, string text, string language)
     {
       // "https://languagetool.org/api/v2"
